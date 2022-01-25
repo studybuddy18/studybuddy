3ab94393ca3028c721f4cda3134ba76148c9f5ac
Study <98375230+studybuddy18@users.noreply.github.com>
2022-01-25 15:49:55 +0800

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","","sourcecodester_hoteldb") or die(mysql_error());
+$con = mysqli_connect("remotemysql.com","1yD5boygH9","nXJunUBszb","1yD5boygH9") or die(mysql_error());
 
 ?>
